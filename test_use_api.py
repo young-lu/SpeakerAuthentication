a1edea6a9ff0adab36d9e143fb6e81718455be01
Connor <cyl28@georgetown.edu>
2018-04-03 12:50:46 -0400

    removed files outside Azure

--- test_use_api.py
+++
@@ -1,8 +0,0 @@
-import json, requests
-
-# Get the response from the API endpoint.
-response = requests.get("http://api.open-notify.org/astros.json").json()
-data = response
-# print(response)
-# 9 people are currently in space.
-print(data)
